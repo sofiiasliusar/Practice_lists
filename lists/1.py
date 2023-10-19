10b70188a6d109b0eeafb83fcdcb24546679d241
Sofiia Sliusar <144224845+sofiiasliusar@users.noreply.github.com>
2023-10-19 13:21:22 +0300

    Add files via upload

---
+++ lists/1.py
@@ -0,0 +1,3 @@
+import random
+numbers = [random.randint(30,90) for _ in range(19)] #список, генератор випадкового числа, цикл, що повторюється 19 разів
+print(numbers)
---
+++ lists/4.py
@@ -0,0 +1,32 @@
+# 1
+n = [3,17,500,342, 33, 2000, 2999999]
+print(n)
+
+# 2
+L = [3, 6, 7]
+n.extend(L)
+print(n)
+
+# 3
+n[2] = 33333
+print(n)
+
+# 4
+n.reverse()
+print(n)
+
+# 5
+n.append(3)
+print(n)
+
+# 6
+n.remove(3)
+print(n)
+
+# 7
+n.sort()
+print(n)
+
+# 8
+n.clear()
+print(n)
